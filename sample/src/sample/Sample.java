e5d54d9cf49f9315af573993fb22fe11115df4f8
tj-bu-706-12 <tj-bu-706-12@DESKTOP-T9PIJ58>
2025-11-03 11:17:32 +0900

    day11

---
+++ sample/src/sample/Sample.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// 자동 완성 : ctrl + space
+		System.out.println("Hello Java~!");
+		// 프로그램 실행 : ctrl + F11
+	}
+
+}
