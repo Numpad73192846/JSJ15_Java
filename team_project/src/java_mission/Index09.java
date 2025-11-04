c941902eff4b9cc578f4a459e3a11e9ac7866aca
tj-bu-706-12 <tj-bu-706-12@DESKTOP-T9PIJ58>
2025-11-04 18:21:41 +0900

    day12

--- team_project/src/java_mission/Index09.java
+++ team_project/src/java_mission/Index09.java
@@ -1,14 +1,17 @@
 package java_mission;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class Index09 {
 	
 	public static String Name(String str) {
-		String answer = "";
-		String[] name = answer.split(" ");
-		return name[0];
+		String person = "";
+		String[] name = str.split(" ");
+		person = name[0];
+		return person;
 	}
 	
 	public static int Money(String str) {
@@ -21,15 +24,61 @@ public class Index09 {
 		return Integer.parseInt(answer);
 	}
 	
-	public static void main(String[] args) {
+	public List<String> personlist() {
+		List<String> personList = new ArrayList<String>();
 		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		
-		System.out.println(Index09.Money(str));
-		System.out.println(Index09.Name(str));
-		System.out.println("성명 출장비 오만원 만원 오천원 천원 오백원 백원 오십원 십원 오원 일원");
+		while(true) {
+			String str = sc.nextLine();
+			personList.add(str);
+			if(str.contains("QUIT")) {
+				personList.removeLast();
+				break;
+			}
+		}
 		
 		sc.close();
+		return personList;
 	}
-
+	
+	public static void main(String[] args) {
+		Index09 index09 = new Index09();
+		List<String> personlist = index09.personlist();
+		String arr[] = new String[personlist.size()];
+		String result[] = personlist.toArray(arr);
+		
+		int money = 50000;
+		int countM = 0;
+		int count = 0;
+		int input;
+		int sum[] = new int[10];
+		boolean sw = true;
+		
+		System.out.println("성명 출장비\t오만원\t만원\t오천원\t천원\t오백원\t백원\t오십원\t십원\t오원\t일원");
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(Index09.Name(result[i]) + " " + Index09.Money(result[i]) + "\t");
+			input = Index09.Money(result[i]);
+			while(money >= 1) {
+				countM = input / money;
+				sum[count] += countM;
+				System.out.print(countM + "\t");
+				input = input % money;
+				if ( sw == true ) {
+					money = money / 5;
+				}
+				else if ( sw == false ) {
+					money = money / 2;
+				}
+				sw = !sw;
+				count++;
+			}
+			money = 50000;
+			count = 0;
+			System.out.println();
+		}
+		System.out.print("전체화폐매수:\t");
+		for (int i = 0; i < 10; i++) {
+			System.out.print(sum[i] + "\t");
+		}
+		
+	}
 }
