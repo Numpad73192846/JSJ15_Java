71ce89a9e067beb92722f61b7493d64ffb2b5d23
Numpad73192846 <seongjun3262@gmail.com>
2025-12-01 18:22:00 +0900

    day31

--- team_project/src/example_gpt/Index11/Bank.java
+++ team_project/src/example_gpt/Index11/Bank.java
@@ -9,7 +9,7 @@ public class Bank {
 	final long MAX_BANK_AMOUNT = 1000000000000L;
 	final int MAX_ACCOUNT = 1000;
 	
-	private static List<Account> accountList = new ArrayList<>();
+	private List<Account> accountList = new ArrayList<>();
 	private long totalAmount = 0L;
 	
 	public Bank() {
@@ -17,18 +17,41 @@ public class Bank {
 	}
 	
 	public Account createAccount(String accountNum, String name, long money, String passward) {
+		if ( accountList.size() > MAX_ACCOUNT ) {
+			System.out.println("생성 가능한 계좌 수를 초과하였습니다.");
+			return null;
+		}
+		if ( money < 0 ) {
+			System.out.println("최초 입금액은 0원 이상만 가능합니다.");
+			return null;
+		}
+		if ( totalAmount + money > MAX_BANK_AMOUNT ) {
+			System.out.println("은행이 보관 가능한 금액을 초과하였습니다.");
+			return null;
+		}
 		Account account = new Account(accountNum, name, money, passward);
 		accountList.add(account);
-		
+		totalAmount += money;
 		return account;
 	}
 	
 	public Account findAccount(String accountNum) {
+		for (Account acc : accountList) {
+			if (acc.getAccountNum().equals(accountNum)) {
+				return acc;
+			}
+		}
 		return null;
 	}
 	
     public boolean deposit(String num, int money) {
-    	return null;
+    	for (Account account : accountList) {
+			if ( account.getAccountNum().equals(num) ) {
+				account.deposit(money);
+				totalAmount += money;
+			}
+		}
+    	return false;
     }
 
     public boolean withdraw(String num, int money, String pw) {
